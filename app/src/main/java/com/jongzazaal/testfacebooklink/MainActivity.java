b31605ab7f8a27a87439f3f25102deefe7297ffe
suchaj.jong@gmail.com <RuikaLism Sathika>
2017-08-13 10:54:27 +0700

    Initial commit

---
+++ app/src/main/java/com/jongzazaal/testfacebooklink/MainActivity.java
@@ -0,0 +1,32 @@
+package com.jongzazaal.testfacebooklink;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView fb = (TextView)findViewById(R.id.fb);
+
+        fb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Uri uri = Uri.parse("https://www.facebook.com/");
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+//                Uri uri = Uri.parse("https://www.facebook.com");
+//                startActivity(new Intent(Intent.ACTION_VIEW, uri));
+            }
+        });
+
+    }
+}
